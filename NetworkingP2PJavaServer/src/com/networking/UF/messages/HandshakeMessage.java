b552dfc5678c698b200fe3f4ba5c0c1fd1a26e0a
chausen <clayhausen@gmail.com>
2016-10-18 23:17:40 -0400

    Added HandshakeMessage and RegularMessage. Just PODs.

--- NetworkingP2PJavaServer/src/com/networking/UF/messages/HandshakeMessage.java
+++ NetworkingP2PJavaServer/src/com/networking/UF/messages/HandshakeMessage.java
@@ -1,14 +1,24 @@
 package com.networking.UF.messages;
 
+import com.google.common.primitives.Ints;
+import com.google.common.primitives.Bytes;
 import com.networking.UF.*;
 
 public class HandshakeMessage {
-	private String handshakeHeader = "";
+	private String handshakeHeader = "P2PFILESHARINGPROJ";
 	byte[] zeroBytes = new byte[10];
 	int peerId = 0;
 
-	public HandshakeMessage(String handshakeHeader, int peerId) {
-		this.handshakeHeader = handshakeHeader;
+	public HandshakeMessage(int peerId) {
 		this.peerId = peerId;
+	}
+
+	public byte[] toByteArray() {
+		byte[] handshakeMessageByteArray = new byte[32];
+		byte[] headerBytes = handshakeHeader.getBytes();
+		byte[] peerIdBytes = Ints.toByteArray(peerId);
+
+		handshakeMessageByteArray = Bytes.concat(headerBytes, zeroBytes, peerIdBytes);
+		return handshakeMessageByteArray;
 	}
 }
--- NetworkingP2PJavaServer/src/com/networking/UF/messages/RegularMessage.java
+++ NetworkingP2PJavaServer/src/com/networking/UF/messages/RegularMessage.java
@@ -1,1 +1,22 @@
 package com.networking.UF.messages;
+
+import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Ints;
+
+public class RegularMessage {
+    private int messageLength;
+    private int messageType;
+    private byte[] messagePayload;
+
+    public RegularMessage(int messageLength, int messageType, byte[] messagePayload) {
+        this.messageLength = messageLength;
+        this.messageType = messageType;
+        this.messagePayload = messagePayload;
+    }
+
+    public byte[] toByteArray() {
+        byte[] messageLengthBytes = Ints.toByteArray();
+
+        return Bytes.concat(messageLengthBytes, Ints.toByteArray(messageType), messagePayload);
+    }
+}
