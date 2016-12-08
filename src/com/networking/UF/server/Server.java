6219264a262b810edb01b81e5d95e67437cabbed
chausen <clayhausen@gmail.com>
2016-12-07 22:16:08 -0500

    changed print

--- src/com/networking/UF/server/Server.java
+++ src/com/networking/UF/server/Server.java
@@ -293,7 +293,8 @@ public class Server implements Runnable {
 						ConnectionState connectionState = myServer.getConnectionState(p2pProtocol.getConnectedPeerId());
 						p2pProtocol.reset();
 						
-						System.out.println("Connected peer: " + p2pProtocol.getConnectedPeerId() + " is waiting: " + connectionState.isWaiting());
+						System.out.println("Connected peer: " + p2pProtocol.getConnectedPeerId());
+//								+ " is waiting: " + connectionState.isWaiting());
 						
 						if (connectionState == null || connectionState.isWaiting()) {
 							System.out.println("Inside the connection state null block.");
